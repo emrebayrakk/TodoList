976a6e8f0dc807afd58c9194cbd15c173d973bd6
emre <eemmrree0@outlook.com>
2021-11-21 18:47:42 +0300

    Business katmanında INoteService ve IUserService interface'leri oluşturuldu.

---
+++ TodoList.Business/Abstract/INoteService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Core.Utilities.Results.Abstract;
+using TodoList.Entities.Concrete;
+using TodoList.Entities.DTOs;
+
+namespace TodoList.Business.Abstract
+{
+    public interface INoteService
+    {
+        IDataResult<List<Note>> GetAll();
+
+        IDataResult<List<Note>> GetAllDayNote(DateTime dateTime);
+
+        IDataResult<List<Note>> GetAllWeekNote(DateTime dateTime);
+        IDataResult<List<Note>> GetAllMonthNote(DateTime dateTime);
+
+        IDataResult<List<NoteDetailDto>> GetNoteDetails();
+        IDataResult<Note> GetById(int noteId);
+        IResult AddNote(Note note);
+    }
+}
---
+++ TodoList.Business/Abstract/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Core.Utilities.Results.Abstract;
+using TodoList.Entities.Concrete;
+
+namespace TodoList.Business.Abstract
+{
+    public interface IUserService
+    {
+        IDataResult<List<User>> GetAll();
+        IResult Add(User user);
+        IResult Delete(User user);
+        IResult Update(User user);
+    }
+}

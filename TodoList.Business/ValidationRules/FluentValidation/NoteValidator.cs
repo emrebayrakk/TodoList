8f86062591fbd18ab6b9e03b5433d21d5f6f82ee
emre <eemmrree0@outlook.com>
2021-11-21 18:48:38 +0300

    Business katmanında Note ve User sınıfları için Validation kuralları yazıldı.

---
+++ TodoList.Business/ValidationRules/FluentValidation/NoteValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using TodoList.Entities.Concrete;
+
+namespace TodoList.Business.ValidationRules.FluentValidation
+{
+    public class NoteValidator:AbstractValidator<Note>
+    {
+        public NoteValidator()
+        {
+            RuleFor(n => n.TaskTitle).NotEmpty();
+            RuleFor(n => n.TaskTitle).Length(2,30);
+            RuleFor(n => n.TaskDescription).NotEmpty();
+            RuleFor(n => n.TaskStartDate).NotEmpty();
+            RuleFor(n => n.TaskEndDate).NotEmpty();
+
+        }
+    }
+}
---
+++ TodoList.Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using FluentValidation;
+using TodoList.Entities.Concrete;
+
+namespace TodoList.Business.ValidationRules.FluentValidation
+{
+    public class UserValidator:AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.UserName).NotEmpty();
+            RuleFor(u => u.UserName).MinimumLength(2);
+            RuleFor(u => u.UserEmail).NotEmpty();
+            RuleFor(u => u.UserEmail).EmailAddress();
+            RuleFor(u => u.UserPassword).NotEmpty();
+            RuleFor(u => u.UserPassword).MinimumLength(6).WithMessage("Parolanız en az 6 karakter içermelir.");
+            RuleFor(u => u.UserPassword).Must(IsPasswordValid).WithMessage("Parolanızda en az bir küçük harf bir büyük harf ve rakam olmalıdır.");
+
+        }
+        private bool IsPasswordValid(string arg)
+        {
+            try
+            {
+                var regex = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[0-9])[A-Za-z\d]");
+                return regex.IsMatch(arg);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}

db21d5236c721dc0e6af7dbe571e4ff55891895d
emre <eemmrree0@outlook.com>
2021-11-21 18:52:17 +0300

    Entities katmanında Note ve User sınıfları oluşturuldu.

---
+++ TodoList.Entities/Concrete/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Core.Entities.Abstract;
+
+namespace TodoList.Entities.Concrete
+{
+    public class Note : IEntity
+    {
+        public int TaskId { get; set; }
+        public int UserId { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime TaskStartDate { get; set; }
+        public DateTime TaskEndDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ TodoList.Entities/Concrete/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Core.Entities.Abstract;
+
+namespace TodoList.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+    }
+}

5281d9b8ef93404a34cdcd08949e5c49800e4c15
emre <eemmrree0@outlook.com>
2021-11-21 18:51:29 +0300

    Core katmanında Result sınıfları ve interfaceleri oluşturuldu.

---
+++ TodoList.Core/Utilities/Results/Concrete/SuccessResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Core.Utilities.Results.Abstract;
+
+namespace TodoList.Core.Utilities.Results.Concrete
+{
+    public class SuccessResult:Result
+    {
+        public SuccessResult(string message) : base(true, message) { }
+
+        public SuccessResult() : base(true) { }
+    }
+}

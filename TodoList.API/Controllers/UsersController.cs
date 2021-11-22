1825ccf6999a057a7a6f8cb453d2e4b5e38b59fd
emre <eemmrree0@outlook.com>
2021-11-22 16:02:06 +0300

    API'de User ekleme, silme, güncelleme ve  listeleme yapıldı

---
+++ TodoList.API/Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Business.Abstract;
+using TodoList.Entities.Concrete;
+
+namespace TodoList.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAllUser()
+        {
+            var result = _userService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("useradd")]
+        public IActionResult UserAdd(User user)
+        {
+            var result = _userService.AddUser(user);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("userdelete")]
+        public IActionResult UserDelete(User user)
+        {
+            var result = _userService.DeleteUser(user);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("userupdate")]
+        public IActionResult UserUpdated(User user)
+        {
+            var result = _userService.UpdateUser(user);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+    }
+}
